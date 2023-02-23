a862d58ba2c6a9f29a93741c31ef84c81431f90e
Aufff <99931123+aufaazka@users.noreply.github.com>
2023-02-23 10:23:06 +0700

    update photo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "235e3b03dd36f3c7"; }
+/******/ 		__webpack_require__.h = function() { return "a1740c9c39a2cedc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
